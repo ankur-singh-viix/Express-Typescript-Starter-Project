66705cdefc214118f31dcc952d9ceddc1663fceb
Ankur Singh <ankurdakshprajapati@example.com>
2025-12-19 23:09:12 +0530

    After removing extra code

--- src/controllers/submission.controller.ts
+++
@@ -1,9 +0,0 @@
-// TODO: Implement the submission controller
-
-export async function createSubmission() {
-    // Implementation goes here
-    console.log("Submission created");
-    return;
-
-    
-}
