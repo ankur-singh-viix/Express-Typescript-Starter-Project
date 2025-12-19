31daae2ed2e182711ff7282db2a317aa964e7f30
Ankur Singh <ankurdakshprajapati@example.com>
2025-12-19 13:31:09 +0530

    SubmissionService now ready

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 
 type ServerConfig = {
     PORT: number,
-    DB_URL: string
+    DB_URL: string,
+    PROBLEM_SERVICE: string
 }
 
 function loadEnv() {
@@ -15,5 +16,6 @@ loadEnv();
 
 export const serverConfig: ServerConfig = {
     PORT: Number(process.env.PORT) || 3001,
-    DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/LC_ProblemService'
+    DB_URL: process.env.DB_URL || "mongodb://localhost:27017/LC_SubmissionService_db",
+    PROBLEM_SERVICE: process.env.PROBLEM_SERVICE || "http://localhost:3001/api/v1"
 };
---
+++ src/controllers/submission.controller.ts
@@ -0,0 +1,9 @@
+// TODO: Implement the submission controller
+
+export async function createSubmission() {
+    // Implementation goes here
+    console.log("Submission created");
+    return;
+
+    
+}
